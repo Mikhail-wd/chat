567e9a099aea9197c99b55e1e8d57d04207f2868
Mikhail-wd <supalis913@yahoo.com>
2026-05-28 13:07:16 +0700

    reworking UI 0.001

--- src/components/chatBox/input.tsx
+++ src/components/chatBox/input.tsx
@@ -65,34 +65,34 @@ export default function Input() {
         setEmojiState(!emojiState)
     }
 
-    function assisInput() {
-        if (chatMsg.length !== 0 && !chatMsg.startsWith("https://")) {
-            setChatMsg("")
+    // function assisInput() {
+    //     if (chatMsg.length !== 0 && !chatMsg.startsWith("https://")) {
+    //         setChatMsg("")
 
-            axios.post(import.meta.env.VITE_SERVER + "chat-api/message",
-                JSON.stringify({ name: context.data.user_name, message: chatMsg, image_link: null, user_color: context.data.userColor, selected_users: context.data.selected_users_list }),
-                {
-                    headers: {
-                        'Content-Type': 'text/json'
-                    }
-                }
-            ).then(() => context.dispatch({ type: "delete_all_selected_users" })
-            ).catch(() => console.warn("ServerError"))
-        } else if (chatMsg.startsWith("https://") && chatMsg.length !== 0) {
-            setChatMsg("")
-            if (chatMsg.endsWith(".jpeg") || chatMsg.endsWith(".jpg") || chatMsg.endsWith(".png") || chatMsg.endsWith(".gif") || chatMsg.endsWith(".webp") || chatMsg.endsWith(".svg")) {
-                axios.post(import.meta.env.VITE_SERVER + "chat-api/message",
-                    JSON.stringify({ name: context.data.user_name, message: "images", image_link: chatMsg, selected_users: context.data.selected_users_list }),
-                    {
-                        headers: {
-                            'Content-Type': 'text/json'
-                        }
-                    }
-                ).then(() => context.dispatch({ type: "delete_all_selected_users" })
-                ).catch(() => console.warn("ServerError"))
-            }
-        }
-    }
+    //         axios.post(import.meta.env.VITE_SERVER + "chat-api/message",
+    //             JSON.stringify({ name: context.data.user_name, message: chatMsg, image_link: null, user_color: context.data.userColor, selected_users: context.data.selected_users_list }),
+    //             {
+    //                 headers: {
+    //                     'Content-Type': 'text/json'
+    //                 }
+    //             }
+    //         ).then(() => context.dispatch({ type: "delete_all_selected_users" })
+    //         ).catch(() => console.warn("ServerError"))
+    //     } else if (chatMsg.startsWith("https://") && chatMsg.length !== 0) {
+    //         setChatMsg("")
+    //         if (chatMsg.endsWith(".jpeg") || chatMsg.endsWith(".jpg") || chatMsg.endsWith(".png") || chatMsg.endsWith(".gif") || chatMsg.endsWith(".webp") || chatMsg.endsWith(".svg")) {
+    //             axios.post(import.meta.env.VITE_SERVER + "chat-api/message",
+    //                 JSON.stringify({ name: context.data.user_name, message: "images", image_link: chatMsg, selected_users: context.data.selected_users_list }),
+    //                 {
+    //                     headers: {
+    //                         'Content-Type': 'text/json'
+    //                     }
+    //                 }
+    //             ).then(() => context.dispatch({ type: "delete_all_selected_users" })
+    //             ).catch(() => console.warn("ServerError"))
+    //         }
+    //     }
+    // }
     function sendMessage(value: React.FormEvent) {
         value.preventDefault()
         if (chatMsg.length !== 0 && !chatMsg.startsWith("https://")) {
