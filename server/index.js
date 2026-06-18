47b63176bd48f47c6211facd5efe1682b05a3b6c
Mikhail-wd <supalis913@yahoo.com>
2026-06-18 15:52:55 +0700

    rework server

--- server/index.js
+++ server/index.js
@@ -88,7 +88,7 @@ const server = Server.createServer((req, res) => {
         res.writeHead(200, {
             "Connection": "keep-alive",
             'Content-Type': 'text/event-stream',
-            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Origin": "https://chat-client-nine-mu.vercel.app",
             "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
             "Cache-Control": "no-cache",
         });
@@ -98,7 +98,7 @@ const server = Server.createServer((req, res) => {
     else if (reqUrl === "/chat-api/init-get-messages") {
         res.writeHead(200, {
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Origin": "https://chat-client-nine-mu.vercel.app",
             "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
         });
@@ -197,7 +197,7 @@ const server = Server.createServer((req, res) => {
         )
         res.writeHead(200, {
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Origin": "https://chat-client-nine-mu.vercel.app",
             "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
         });
@@ -214,8 +214,8 @@ const server = Server.createServer((req, res) => {
         res.writeHead(200, {
             "Connection": "keep-alive",
             'Content-Type': 'text/event-stream',
-            // "Access-Control-Allow-Origin": "https://chat-eta-one-89.vercel.app"
-            // "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+            "Access-Control-Allow-Origin": "https://chat-client-nine-mu.vercel.app",
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
             // "Cache-Control": "no-cache",
         });
         EventEmitter.addListener("add_user", () => res.write(`data:${JSON.stringify(usersList)}\n\n`))
@@ -227,7 +227,7 @@ const server = Server.createServer((req, res) => {
         )
         res.writeHead(200, {
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Origin": "https://chat-client-nine-mu.vercel.app",
             "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
         });
@@ -247,7 +247,7 @@ const server = Server.createServer((req, res) => {
         )
         res.writeHead(200, {
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Origin": "https://chat-client-nine-mu.vercel.app",
             "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
         });
